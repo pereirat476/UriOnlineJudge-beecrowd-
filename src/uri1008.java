0ab377eac3fa2b9522d3c6258a167d391ed6ae26
Tiago Santos <pereirat476@gmail.com>
2022-07-12 09:36:10 -0300

    uri1008

---
+++ src/uri1008.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class uri1008 {
+    /*Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas,
+    o valor que recebe por hora e calcula o salário desse funcionário.
+    A seguir, mostre o número e o salário do funcionário, com duas casas decimais.*/
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        int NumeroFuncionario = sc.nextInt();
+        int HorasTrabalhadas = sc.nextInt();
+        double ValorPorHoraTrabalhada = sc.nextDouble();
+
+        double SalarioFinal = HorasTrabalhadas * ValorPorHoraTrabalhada;
+
+        System.out.println("NUMBER = " + NumeroFuncionario);
+        System.out.println("SALARY = " + SalarioFinal);
+    }
+}
