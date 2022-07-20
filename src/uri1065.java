93185b875a00eb31c381441455aa542eb87505ab
Tiago Santos <pereirat476@gmail.com>
2022-07-20 09:09:45 -0300

    uri1065

--- src/uri1065.java
+++ src/uri1065.java
@@ -4,6 +4,16 @@ public class uri1065 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int x;
+        int totalPares = 0;
+
+        for(int i = 0;i < 5; i++){
+            x = sc.nextInt();
+            if(x%2==0){
+                totalPares += 1;
+            }
+        }
+        System.out.println(totalPares + " valores pares");
         sc.close();
     }
 }
