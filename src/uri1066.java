d71cff0c63ec84f62d93f1470894b6656ee8c05e
Tiago Santos <pereirat476@gmail.com>
2022-07-20 10:53:45 -0300

    uri1066

---
+++ src/uri1066.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class uri1066 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int contPar = 0;
+        int contImpar = 0;
+        int contPositivos = 0;
+        int contNegativos = 0;
+        int x;
+        for (int i = 0; i < 5; i++) {
+            x = sc.nextInt();
+            if (x % 2 == 0) contPar++;
+            else contImpar++;
+            if (x > 0) contPositivos++;
+            else if(x < 0) contNegativos++;
+        }
+        System.out.println(contPar + " valor(es) par(es)");
+        System.out.println(contImpar + " valor(es) impar(es)");
+        System.out.println(contPositivos + " valor(es) positivo(s)");
+        System.out.println(contNegativos + " valor(es) negativo(s)");
+    }
+
+}
