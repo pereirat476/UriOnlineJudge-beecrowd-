93b8f30cfcfdf2627af3f35d53f3e7b5f8d77d17
Tiago Santos <pereirat476@gmail.com>
2022-07-21 09:41:01 -0300

    ur1078

---
+++ src/uri1078.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class uri1078 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        for(int i = 1;i <= 10;i++){
+            int produto = i * N;
+            System.out.println(i + " x " + N + " = " + produto);
+        }
+
+        sc.close();
+    }
+}
